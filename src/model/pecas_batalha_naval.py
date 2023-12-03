84cd830080d5645bb88ad14cdfb6087c67b60103
MatheusDecarliGuarnieri <mdg2503@gmail.com>
2023-12-03 18:20:33 -0300

    arruamdno ambientação do codigo do tabuleiro

--- src/model/pecas_batalha_naval.py
+++ src/model/pecas_batalha_naval.py
@@ -1,17 +1,17 @@
 class PecasBatalhaNaval:
     def __init__(self):
         self.tipos_embarcacoes = {
-            'Submarino': ['S'],
-            'Destroier': ['D1', 'D2'],
+            'Submarino': ['S1'] ,
+            'Destroier': ['D1', 'D2'] ,
             'Cruzador': ['C1', 'C2', 'C3'],
             'Encouracado': ['E1', 'E2', 'E3', 'E4'],
-            'PortaAvioes': ['P1','P2','P3','P4','P5']
+            'PortaAvioes': ['P1', 'P2', 'P3', 'P4', 'P5']
         }
 
     def obter_peca(self, nome_peca):
         return self.tipos_embarcacoes.get(nome_peca, None)
 
-def adicionar_peca_portaavioes(self, tabuleiro, linha, coluna):
+    def adicionar_peca_portaavioes(self, tabuleiro, linha, coluna):
         # Verifica se a posição está dentro dos limites do tabuleiro
         if 0 <= linha < len(tabuleiro) and 0 <= coluna < len(tabuleiro[0]):
             # Adiciona a peça 'P' em formato de T
--- src/view/tabuleiroview.py
+++ src/view/tabuleiroview.py
@@ -1,7 +1,19 @@
 from typing import List
 
 class TabuleiroView:
+
     @staticmethod
     def formatar_tabuleiro(tabuleiro: List[List[str]]) -> List[str]:
-        formatted_tabuleiro = [' '.join(linha) for linha in tabuleiro]
+        formatted_tabuleiro = []
+
+        for linha in tabuleiro:
+            formatted_linha = []
+            for item in linha:
+                if isinstance(item, list):
+                    # Se o item for uma lista, unimos seus elementos em uma string
+                    formatted_linha.append(' '.join(item))
+                else:
+                    formatted_linha.append(item)
+            formatted_tabuleiro.append(' '.join(formatted_linha))
+
         return formatted_tabuleiro
